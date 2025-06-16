b4fc7963a1225834eaf2cc7238c0cddd77696f4d
Shubham kumar <shubhamkumar818845@gmail.com>
2025-06-16 13:39:33 +0530

    connection commit added

---
+++ src/index.js
@@ -0,0 +1,60 @@
+// require ('dotenv').config({path:'./env'}) 
+// uper vli line se code chal jayega pr ek better aporoch hai below
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants";
+
+import express from "express"
+const app = express()
+
+// function connectDB(){}
+
+
+// connectDB()
+
+// uper vale function se better hai effe uske phle semicollen use krte hai for reducing erorr(cleaning purpuse)
+
+;(async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("ERROR")
+        throw err
+       })
+    } catch (error) {
+        console.log("ERROR",error);
+        throw err
+        
+    }
+})() */
