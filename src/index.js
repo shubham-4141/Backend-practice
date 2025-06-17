4f6857404701030daf5da65595562075052e2c60
Shubham kumar <shubhamkumar818845@gmail.com>
2025-06-17 13:17:58 +0530

    middleware added

--- src/index.js
+++ src/index.js
@@ -14,6 +14,16 @@ dotenv.config({
 
 
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000,() => {
+            console.log(` * Server is running at port : ${process.env.PORT}`);
+            
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connectin failed !!!", err);
+    
+})
 
 
 
